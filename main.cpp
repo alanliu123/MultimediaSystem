301ac68ce9fcf26d31da284575ca7bbfed5cd28c
alanliu123 <liufei0428@hotmail.com>
2018-09-19 15:59:45 +0800

    normal

--- main.cpp
+++ main.cpp
@@ -4,7 +4,16 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     MainWindow w;
+
+    QImage imagebackground;
+    imagebackground.load(":/img/background.jpg");
+
+    QPalette palette;
+    palette.setBrush(w.backgroundRole(), QBrush(imagebackground));
+    w.setPalette(palette);
+    w.setAutoFillBackground(true);
     w.show();
 
     return a.exec();
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,18 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->verticalWidgetleft->setStyleSheet("background-color:#ffffff");
+    ui->verticalWidgetright->setStyleSheet("background-color:#ffffff");
+
+    ui->usertitle->setStyleSheet("background-color:#dddddd");
+
+    QImage *userhead_img = new QImage(":/img/man.png");
+    QImage *scaled_userhead_img = new QImage();
+    *scaled_userhead_img = userhead_img->scaled(70,70,Qt::KeepAspectRatio);
+    ui->userhead->setPixmap(QPixmap::fromImage(*scaled_userhead_img));
+    ui->userhead->setAlignment(Qt::AlignCenter);
+    ui->userhead->setStyleSheet("background:transparent");
 }
 
 MainWindow::~MainWindow()
